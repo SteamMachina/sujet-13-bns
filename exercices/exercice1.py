cd54b81adf9eefbfd35dfcea55423e68909c98a8
SteamMachina <52200491+SteamMachina@users.noreply.github.com>
2021-02-10 14:44:46 +0100

    Update exercice1.py

--- exercices/exercice1.py
+++ exercices/exercice1.py
@@ -1,2 +1,11 @@
-def tri_selection():
-    return ...
+def tri_selection(tab):
+  id = 0
+  for i in range(len(tab)):
+    id = i
+    for j in range(i, len(tab)):
+      if tab[j] < tab[id]:
+        id = j
+    temp = tab[i]
+    tab[i] = tab[id]
+    tab[id] = temp
+  return tab
